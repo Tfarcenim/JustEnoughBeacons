8ebce752e27bc2f6d300cec1fb5ee9b560fbab67
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-04-03 17:56:14 -0400

    forge should not be doing breaking changes after an rb

--- src/main/java/com/tfar/justenoughbeacons/jei/conduitblock/ConduitBaseRecipe.java
+++ src/main/java/com/tfar/justenoughbeacons/jei/conduitblock/ConduitBaseRecipe.java
@@ -1,7 +1,9 @@
 package com.tfar.justenoughbeacons.jei.conduitblock;
 
+import com.tfar.justenoughbeacons.jei.beaconblock.BeaconBlockRecipe;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,10 +11,18 @@ import java.util.stream.Collectors;
 public class ConduitBaseRecipe {
 
   public static List<ItemStack> getConduitBlocks(){
-    return Tags.Blocks.SUPPORTS_CONDUIT
-            .getAllElements()
-            .stream()
-            .map(ItemStack::new)
-            .collect(Collectors.toList());
+    return ForgeRegistries.BLOCKS.getValues().stream()
+            .filter(block ->
+                    {
+                      try {
+                        return block.isConduitFrame(
+                                block.getDefaultState(), null, null, null);
+                      } catch (NullPointerException specialblock){
+                        BeaconBlockRecipe.special.add(block);
+                        return false;
+                      }
+                    }
+            )
+            .map(ItemStack::new).collect(Collectors.toList());
   }
 }
